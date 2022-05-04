a9e5fc636a2e484ada9abafb93e359433099bdb4
rckam <rckmiri@gmail.com>
2022-05-04 17:56:30 +0200

    added joining a forum in forum page still need to implement in recommendations

--- codeup/src/app/forum/forum.component.ts
+++ codeup/src/app/forum/forum.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {UserForumRelationService} from "../shared/services/userForumRelationService";
+import {AuthService} from "../shared/services/authService";
 
 @Component({
   selector: 'app-forum',
@@ -8,12 +10,48 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class ForumComponent implements OnInit {
   public forum: any
-  constructor(private route: ActivatedRoute) {
+  public hasJoinned = false;
+
+  constructor(private route: ActivatedRoute, private userForumRelationService: UserForumRelationService, private authService: AuthService) {
     this.forum = route.snapshot.data['forumResolver'];
-    console.log(this.forum)
   }
 
   ngOnInit(): void {
+    this.getUserForumRelation();
   }
 
+  joinOrQuit() {
+
+    if (this.hasJoinned) {
+      console.log("inside")
+      this.userForumRelationService.deleteUserForumRelation(this.forum.id).subscribe(value => {
+        this.hasJoinned = !value;
+      }, error => {
+        this.hasJoinned = true;
+      });
+    } else {
+      console.log("inside else")
+      console.log(this.authService.loggedUser)
+      if (this.authService.loggedUser) {
+        console.log("inside if")
+        this.userForumRelationService.addUserForumRelation(this.forum.id).subscribe(value => {
+            this.hasJoinned = !!value;
+          }
+        );
+      }
+    }
+  }
+
+  getUserForumRelation() {
+    this.userForumRelationService.getUserForumRelation(this.forum.id).subscribe(value => {
+      console.log(value)
+      if (value === null) {
+        this.hasJoinned = false;
+      } else {
+        this.hasJoinned = true;
+      }
+    }, error => {
+      this.hasJoinned = false;
+    })
+  }
 }
--- codeup/src/app/shared/services/userForumRelationService.ts
+++ codeup/src/app/shared/services/userForumRelationService.ts
@@ -6,26 +6,26 @@ import {UserForumRelation} from "../entities/UserForumRelation";
 @Injectable({
   providedIn: 'root'
 })
-export class ForumService {
+export class UserForumRelationService {
 
   constructor(private httpclient: HttpClient) {
   }
 
   public addUserForumRelation(forumId: number): Observable<UserForumRelation> {
     return this.httpclient.post<UserForumRelation>(
-      'http://localhost:8080/user_forum_relation/add/forum/' + forumId, null
+      'http://localhost:8080/user-forum-relation/add/forum/' + forumId, {}
     );
   }
 
   public getUserForumRelation(forumId: number): Observable<UserForumRelation> {
     return this.httpclient.get<UserForumRelation>(
-      'http://localhost:8080/user_forum_relation/forum/' + forumId
+      'http://localhost:8080/user-forum-relation/forum/' + forumId
     );
   }
 
   public deleteUserForumRelation(forumId: number): Observable<boolean> {
     return this.httpclient.delete<boolean>(
-      'http://localhost:8080/user_forum_relation/delete/forum/' + forumId,
+      'http://localhost:8080/user-forum-relation/delete/forum/' + forumId, {}
     );
   }
 
