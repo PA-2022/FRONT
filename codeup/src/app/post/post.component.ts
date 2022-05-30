2fd3876662bdb7ebdc4e2f55bcd28ecefc7332e7
rckam <rckmiri@gmail.com>
2022-05-30 16:46:56 +0200

    added comments*

--- codeup/src/app/post/post.component.ts
+++ codeup/src/app/post/post.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {Forum} from "../shared/entities/Forum";
+import {Comment} from "../shared/entities/Comment";
 import {ActivatedRoute} from "@angular/router";
 import {Post} from "../shared/entities/Post";
 import {UserService} from "../shared/services/userService";
 import {ForumService} from "../shared/services/forumService";
+import {PostService} from "../shared/services/postService";
+import {AuthService} from "../shared/services/authService";
 
 @Component({
   selector: 'app-post',
@@ -14,8 +16,16 @@ export class PostComponent implements OnInit {
   post: Post;
   user: any;
   forum: any;
+  comments: any;
+  commentValue: String = "";
+  respondId: any;
+  respondUsername: string | undefined;
+  public loggedUser = this.authService.loggedUser;
+  editMessage: any | null | undefined;
+  commentCount: any;
 
-  constructor(private route: ActivatedRoute, private userService: UserService, private forumService: ForumService) {
+
+  constructor(private route: ActivatedRoute, private userService: UserService, private forumService: ForumService, private postService: PostService, private authService: AuthService) {
     this.post = route.snapshot.data['postResolver'];
     this.userService.getUserById(Number(this.post.userId)).subscribe(value => {
       this.user = value
@@ -23,9 +33,77 @@ export class PostComponent implements OnInit {
     this.forumService.getForumById(Number(this.post.forumId)).subscribe(value => {
       this.forum = value
     });
+    this.getComments();
   }
 
   ngOnInit(): void {
   }
 
+  scrollToElement($element: any): void {
+    $element.scrollIntoView({behavior: "smooth", block: "end", inline: "end"});
+  }
+
+  getComments(): void {
+    this.postService.getPostComments(Number(this.post.id)).subscribe(value => {
+      this.comments = value;
+      this.getPostCommentCount();
+    })
+  }
+
+
+  sendComment() {
+    if(this.editMessage) {
+      this.editComment();
+      return;
+    }
+
+    if(this.respondUsername){
+      this.commentValue =  '@' + this.respondUsername.toString() + ' ' + this.commentValue.toString();
+    }
+    const comment: Comment = {
+      id: null,
+      content: this.commentValue.toString(),
+      commentParentId: this.respondId ?? null,
+      userId: null,
+      code: null,
+      postId: Number(this.post.id),
+      creationDate: null,
+      lastUpdateDate: null
+    }
+    this.postService.sendComment(comment).subscribe(value => {
+      this.getComments();
+      this.commentValue = "";
+      this.respondUsername = undefined;
+      this.respondId = null;
+    });
+  }
+
+  delete(commentId: number) {
+    if (confirm('Are you sure you want to delete your comment ?')) {
+      this.postService.deleteComment(commentId).subscribe(response =>{
+        this.getComments();
+      });
+    }
+  }
+
+  editSetup(comment: any) {
+    this.editMessage = comment;
+    this.commentValue = this.editMessage.content;
+  }
+
+  editComment(): void {
+    this.editMessage.content = this.commentValue.toString();
+    this.postService.editComment(this.editMessage).subscribe(data => {
+      this.getComments();
+      this.commentValue = '';
+      this.respondId ='';
+      this.respondUsername ='';
+    });
+  }
+
+  getPostCommentCount(): void {
+    this.postService.getCommentCount(Number(this.post.id)).subscribe(data => {
+      this.commentCount = data;
+    })
+  }
 }
--- codeup/src/app/shared/services/postService.ts
+++ codeup/src/app/shared/services/postService.ts
@@ -1,9 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable} from 'rxjs';
-import {UserForumRelation} from "../entities/UserForumRelation";
 import {Post} from "../entities/Post";
-
+import {Comment} from "../entities/Comment";
 @Injectable({
   providedIn: 'root'
 })
@@ -19,7 +18,34 @@ export class PostService {
   }
 
   public getPostById(id: number): Observable<Post> {
-    console.log(id)
     return this.httpclient.get<Post>('http://localhost:8080/posts/' + id);
+  }
+
+  public getPostComments(id: number): Observable<any> {
+    return this.httpclient.get<any>('http://localhost:8080/comments/comment/post/' + id)
+  }
+
+  sendComment(comment: Comment) {
+    return this.httpclient.post<Comment>(
+      'http://localhost:8080/comments' , comment
+    );
+  }
+
+  deleteComment(commentId : number) {
+    return this.httpclient.delete<boolean>(
+      'http://localhost:8080/comments/comment/' + commentId
+    );
+  }
+
+  editComment(comment: any) {
+    return this.httpclient.put<Comment>(
+      'http://localhost:8080/comments' , comment
+    );
+  }
+
+  getCommentCount(postId: number) {
+    return this.httpclient.get<number>(
+      'http://localhost:8080/comments/comment/post/' + postId + '/count'
+    )
   }
 }
